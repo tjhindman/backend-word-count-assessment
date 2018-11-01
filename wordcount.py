00563617af36edfb8c7c9ec5cf8323c79972c050
TJ Hindman <thind131@gmail.com>
2018-11-01 10:10:51 -0400

    wordcount still in progress

--- wordcount.py
+++ wordcount.py
@@ -40,12 +40,28 @@ print_words() and print_top().
 """
 
 import sys
+from collections import Counter
 
 # +++your code here+++
 # Define print_words(filename) and print_top(filename) functions.
 # You could write a helper utility function that reads a file
 # and builds and returns a word/count dict for it.
 # Then print_words() and print_top() can just call the utility function.
+
+
+def print_words(filename):
+    with open(filename) as f:
+        wordcount = Counter(f.read().split())
+
+        for item in sorted(wordcount.items()):
+            print("{}\t{}".format(*item))
+
+
+def print_top(filename):
+    with open(filename) as f:
+        wordcount = Counter(f.read().split())
+
+        # for item in wordcount.items().sort(key=)        
 
 ###
 
